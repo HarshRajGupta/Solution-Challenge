90ffcdde4335469b8b0a1d6778c41533c3609092
punityadav2512 <b520040@iiit-bh.ac.in>
2022-02-02 23:45:08 +0530

    login with google firebase

---
+++ src/app/models/farmer.ts
@@ -0,0 +1,7 @@
+// export interface Farmer {
+//     id: string,
+//     name: string,
+//     email: string,
+//     mobile: string,
+//     password: string
+// }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { GoogleAuthProvider } from '@angular/fire/auth'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore, private fireAuth: AngularFireAuth, private router: Router) { }
+
+
+  login(email: string, password: string) {
+    this.fireAuth.signInWithEmailAndPassword(email, password).then(() => {
+      localStorage.setItem('token', 'true');
+      this.router.navigate(['/home']);
+    }, err => {
+      alert('Something went wrong');
+      this.router.navigate(['/login']);
+    })
+  }
+
+
+
+  logout() {
+    this.fireAuth.signOut().then(() => {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    }, err => {
+      alert(err.message);
+    })
+  }
+
+
+  googleSignIn() {
+    this.fireAuth.signInWithPopup(new GoogleAuthProvider).then((res) => {
+
+      this.router.navigate(['/home']);
+      localStorage.setItem('token', JSON.stringify(res.user?.uid));
+    }, err => {
+      alert(err.message);
+    })
+  }
 }
